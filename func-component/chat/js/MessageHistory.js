b6a31475a3572770e5bb5b68d0adf7f607fcb618
Aleksey Trubtisyn <at.8441451@yandex.ru>
2017-10-04 00:35:43 +0300

    add MessageHistory, chat done

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function MessageHistory({ list }) {
+  if (!list || list.length == 0) return null;
+
+  let messages = list.map( item => {
+    let { text, time } = item;
+    let message = {
+       text,
+       time
+    }
+
+    switch (item.type) {
+      case 'message':
+        return <Message key={ item.id } from={ item.from } message={ message }/>;
+
+      case 'response':
+        return <Response key={ item.id } from={ item.from } message={ message }/>;
+
+      case 'typing':
+        return <Typing key={ item.id } from={ item.from } message={ message }/>;
+
+      default:
+        return null;
+    }
+  });
+
+   return (
+     <ul>
+       { messages }
+     </ul>
+   );
+ }
+
+MessageHistory.defaultProps = {
+  list: []
+}
